361e474ab1bc842c491308c6a696600a2279ea06
Derek Kumagai <Derek.kumagai@elektrobit.com>
2017-10-20 14:36:06 -0700

    initial commit of SVM Training Data Tool

---
+++ tools/SvmTrainingTool/gui/InteractiveGridPanel.java
@@ -0,0 +1,377 @@
+package gui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
+import java.util.HashSet;
+
+import javax.swing.JPanel;
+
+import util.Pair;
+
+public class InteractiveGridPanel extends JPanel
+{
+	public static final Color DEFAULT_GRID_COLOR = Color.YELLOW;
+	public static final Color DEFAULT_SELECTED_CELL_COLOR = new Color(255, 255, 255, 192);
+	
+	private static final long serialVersionUID = 1L;
+	
+	private int m_width;
+	private int m_height;
+	private int m_horizontalCells;
+	private int m_verticalCells;
+	
+	private BufferedImage m_background;
+	private BufferedImage m_frameBuffer;
+	private Color m_gridColor;
+	private Color m_selectedCellColor;
+	private CellSelector m_cellSelector;
+	private Pair<Integer, Integer> m_topLeftRoI;
+	private Pair<Integer, Integer> m_bottomRightRoI;
+	
+	public InteractiveGridPanel()
+	{
+		m_width = 0;
+		m_height = 0;
+		m_horizontalCells = 0;
+		m_verticalCells = 0;
+		m_background = null;
+		m_frameBuffer = null;
+		m_gridColor = DEFAULT_GRID_COLOR;
+		m_selectedCellColor = DEFAULT_SELECTED_CELL_COLOR;
+		m_cellSelector = new CellSelector(this);
+		m_topLeftRoI = null;
+		m_bottomRightRoI = null;
+		
+		this.addMouseListener(m_cellSelector);
+	}
+	
+	public void setBackgroundImageAndRedraw(final BufferedImage img)
+	{
+		m_background = img;	    
+		m_width = img.getWidth();
+		m_height = img.getHeight();
+		
+		//copy background to framebuffer, m_background will be used to regenerate areas painted over
+	    ColorModel color = img.getColorModel();
+	    WritableRaster raster = img.copyData(null);
+	    m_frameBuffer =  new BufferedImage(color, raster, color.isAlphaPremultiplied(), null);	    
+	    
+		//setSize(m_width, m_height);
+		setSize(1536, 864);
+		drawGridOnFrameBuffer();
+		repaint();
+	}
+	
+	public void setGridDimensions(final int horizontalCells, final int verticalCells)
+	{		
+		m_horizontalCells = horizontalCells;
+		m_verticalCells = verticalCells;
+		
+		setRegionOfInterest(new Pair<Integer, Integer>(0, 0),
+				            new Pair<Integer, Integer>(horizontalCells - 1, verticalCells - 1));
+
+		if(m_background != null)
+		{
+			setBackgroundImageAndRedraw(m_background);
+			//drawGridOnFrameBuffer();		
+			//repaint();
+		}
+	}
+	
+	public void setRegionOfInterest(final Pair<Integer, Integer> topLeft,
+			                        final Pair<Integer, Integer> bottomRight)
+	{
+		if(topLeft.getFirst() >= 0 && topLeft.getFirst() < m_horizontalCells &&
+		   topLeft.getSecond() >= 0 && topLeft.getSecond() < m_verticalCells)
+		{
+			if(bottomRight.getFirst() >= 0 && bottomRight.getFirst() < m_horizontalCells &&
+			   bottomRight.getSecond() >= 0 && bottomRight.getSecond() < m_verticalCells)
+			{
+				if(bottomRight.getFirst() > topLeft.getFirst() &&
+				   bottomRight.getSecond() > topLeft.getSecond())
+				{
+					m_topLeftRoI = topLeft;
+					m_bottomRightRoI = bottomRight;
+					
+					if(m_background != null)
+					{
+						setBackgroundImageAndRedraw(m_background);
+					}
+				}
+			}
+		}
+	}
+	
+	public void setGridColor(final Color gridColor)
+	{
+		m_gridColor = gridColor;
+
+		drawGridOnFrameBuffer();
+		repaint();
+	}
+	
+	public int getNumHorizontalCells()
+	{
+		return m_horizontalCells;
+	}
+	
+	public int getNumVerticalCells()
+	{
+		return m_verticalCells;
+	}
+	
+	public int getCellWidth()
+	{
+		return m_width / m_horizontalCells;
+	}
+	
+	public int getCellHeight()
+	{
+		return m_height / m_verticalCells;
+	}
+	
+	public void setSelectedCellColor(final Color selectedCellColor)
+	{
+		m_selectedCellColor = selectedCellColor;
+		
+		eraseAllSelectedCellsOnFrameBuffer();
+		drawAllSelectedCellsOnFrameBuffer();
+		repaint();
+	}
+
+	public Pair<Integer, Integer> getTopLeftRoI()
+	{
+		return this.m_topLeftRoI;
+	}
+	
+	public Pair<Integer, Integer> getBottomRightRoI()
+	{
+		return this.m_bottomRightRoI;
+	}
+
+	public HashSet<Pair<Integer, Integer>> getSelectedCells()
+	{
+		return m_cellSelector.getSelectedCells();
+	}
+	
+	public void resetSelectedCells()
+	{
+		m_cellSelector.getSelectedCells().clear();
+	}
+	
+	@Override
+    public Dimension getPreferredSize()
+    {
+        //return new Dimension(m_width, m_height);
+		//should not be hardcoded
+        return new Dimension(1536, 864);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g)
+    {
+    	super.paintComponent(g);
+    	
+    	if(m_frameBuffer != null)
+    	{        	
+        	g.drawImage(m_frameBuffer.getScaledInstance(this.getWidth(),
+        			                                    this.getHeight(),
+        			                                    Image.SCALE_FAST),
+        			    0,
+        			    0,
+        			    this);
+    	}
+    }
+	
+	private void drawGridOnFrameBuffer()
+	{
+    	if(m_frameBuffer != null)
+    	{
+    		int cellWidth = getCellWidth();
+    	    int cellHeight = getCellHeight();
+        	int regionTopLeftX = m_topLeftRoI.getFirst() * cellWidth;
+        	int regionTopLeftY = m_topLeftRoI.getSecond() * cellHeight;
+        	int regionBotRtX = (m_bottomRightRoI.getFirst() + 1) * cellWidth;
+        	int regionBotRtY = (m_bottomRightRoI.getSecond() + 1) * cellHeight;
+
+    		Graphics2D g2d = m_frameBuffer.createGraphics();
+    		g2d.setColor(m_gridColor);
+    		
+    		for(int x = cellWidth; x < m_width; x += cellWidth)
+    		{
+    			if(x >= regionTopLeftX && x <= regionBotRtX)
+    			{
+        			//g2d.drawLine(x, 0, x, m_height);
+    				g2d.drawLine(x, regionTopLeftY, x, regionBotRtY);
+    			}
+    		}
+    		
+    		for(int y = cellHeight; y < m_height; y += cellHeight)
+    		{
+    			if(y >= regionTopLeftY && y <= regionBotRtY)
+    			{
+    				//g2d.drawLine(0, y, m_width, y);
+    				g2d.drawLine(regionTopLeftX, y, regionBotRtX, y);
+    			}
+    		}
+    	}
+	}
+
+	private void drawSelectedCellOnFrameBuffer(final int column, final int row)
+	{
+    	if(m_frameBuffer != null)
+    	{
+	    	int cellWidth = getCellWidth();
+	    	int cellHeight = getCellHeight();
+	
+			Graphics2D g2d = m_frameBuffer.createGraphics();		
+			g2d.setColor(m_selectedCellColor);		
+			g2d.fillRect((column * cellWidth) + 1,
+			   	         (row * cellHeight) + 1,
+				          cellWidth - 1,
+				          cellHeight - 1);
+    	}
+	}		
+	
+	private void eraseSelectedCellOnFrameBuffer(final int column, final int row)
+	{
+    	if(m_frameBuffer != null)
+    	{			
+	    	int cellWidth = getCellWidth();
+	    	int cellHeight = getCellHeight();		
+
+			Graphics2D g2d = m_frameBuffer.createGraphics();
+			BufferedImage backgroundCell = m_background.getSubimage((column * cellWidth) + 1,
+					                                                (row * cellHeight) + 1,
+					                                                cellWidth - 1,
+					                                                cellHeight - 1);
+			g2d.drawImage(backgroundCell,
+					      null,
+					      (column * cellWidth) + 1,
+					      (row * cellHeight) + 1);
+    	}
+	}	
+
+	private void drawAllSelectedCellsOnFrameBuffer()
+	{
+		for(Pair<Integer, Integer> cell : getSelectedCells())
+		{
+			drawSelectedCellOnFrameBuffer(cell.getFirst(), cell.getSecond());
+		}
+	}
+
+	private void eraseAllSelectedCellsOnFrameBuffer()
+	{
+		for(int col = 0; col < m_horizontalCells; ++col)
+		{
+			for(int row = 0; row < m_verticalCells; ++row)
+			{
+				eraseSelectedCellOnFrameBuffer(col, row);
+			}
+		}
+	}
+    
+    class CellSelector implements MouseListener
+    {
+    	private HashSet< Pair<Integer, Integer> > m_selectedCells;
+    	private InteractiveGridPanel m_parent;
+    	private Pair<Integer, Integer> m_lastClickedCell;
+    	
+    	public CellSelector(InteractiveGridPanel parent)
+    	{
+    		m_selectedCells = new HashSet< Pair<Integer, Integer> >();
+    		m_parent = parent;
+    		m_lastClickedCell = null;
+    	}
+    	
+    	public HashSet< Pair<Integer, Integer> > getSelectedCells()
+    	{
+    		return m_selectedCells;
+    	}
+    	
+    	@Override
+    	public void mouseClicked(MouseEvent e){}
+
+    	@Override
+    	public void mouseEntered(MouseEvent e){}
+
+    	@Override
+    	public void mouseExited(MouseEvent e){}
+
+    	@Override
+    	public void mousePressed(MouseEvent e)
+    	{
+    		m_lastClickedCell = getClickedCell(e); 
+    	}
+
+    	@Override
+    	public void mouseReleased(MouseEvent e)
+    	{
+    		Pair<Integer, Integer> clickedCell = getClickedCell(e);
+    		
+    		if(m_lastClickedCell != null &&
+    		   m_lastClickedCell.getFirst() == clickedCell.getFirst() &&
+    		   m_lastClickedCell.getSecond() == clickedCell.getSecond())
+    		{
+    			toggleCell(m_lastClickedCell);
+    		}
+    		
+    		m_lastClickedCell = null;
+    	}
+    	
+    	private Pair<Integer, Integer> getClickedCell(final MouseEvent e)
+    	{
+    		Pair<Integer, Integer> clickedCell = null;    	    
+
+    		int scaledCellWidth = (int)Math.ceil((double)m_parent.getWidth() / m_parent.getNumHorizontalCells());
+    		int scaledCellHeight = (int)Math.ceil((double)m_parent.getHeight() / m_parent.getNumVerticalCells());
+        	int regionTopLeftX = m_topLeftRoI.getFirst() * scaledCellWidth;
+        	int regionTopLeftY = m_topLeftRoI.getSecond() * scaledCellHeight;
+        	int regionBotRtX = (m_bottomRightRoI.getFirst() + 1) * scaledCellWidth;
+        	int regionBotRtY = (m_bottomRightRoI.getSecond() + 1) * scaledCellHeight;
+        	
+        	if(e.getX() >= regionTopLeftX && e.getX() <= regionBotRtX &&
+               e.getY() >= regionTopLeftY && e.getY() <= regionBotRtY)
+        	{
+	    		if(m_parent.getWidth() > 0 && m_parent.getHeight() > 0)
+	    		{
+		    		int gridXClicked = e.getX() / scaledCellWidth;
+		    		int gridYClicked = e.getY() / scaledCellHeight;
+		    		
+		    		clickedCell = new Pair<Integer, Integer>(gridXClicked, gridYClicked);
+	    		}
+        	}
+    		
+    		return clickedCell;
+    	}
+    	
+    	private void toggleCell(final Pair<Integer, Integer> clickedCell)
+    	{
+    		if(clickedCell != null)
+    		{            	
+        		if(!m_selectedCells.contains(clickedCell))
+        		{        			
+        			m_selectedCells.add(clickedCell);
+        			m_parent.drawSelectedCellOnFrameBuffer(clickedCell.getFirst(),
+        					                               clickedCell.getSecond());
+        			m_parent.repaint();
+        		}
+        		else
+        		{
+        			m_selectedCells.remove(clickedCell);
+        			m_parent.eraseSelectedCellOnFrameBuffer(clickedCell.getFirst(),
+        					                                clickedCell.getSecond());
+        			m_parent.repaint();
+        		}
+    		}    	
+    	}
+    }
+}
---
+++ tools/SvmTrainingTool/gui/SVMTrainingDataGUI.java
@@ -0,0 +1,357 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+
+import javax.swing.AbstractButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.WindowConstants;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import core.SVMTrainingDataToolApplication;
+import util.Pair;
+
+public class SVMTrainingDataGUI
+{	
+	private JFrame m_frame;
+	private InteractiveGridPanel m_gridPanel;
+	private SVMTrainingDataToolApplication m_parentApplication;
+	
+	public SVMTrainingDataGUI(final SVMTrainingDataToolApplication parentApplication)
+	{
+		m_frame = new JFrame();
+		m_gridPanel = new InteractiveGridPanel();
+		m_parentApplication = parentApplication;
+
+		configureLayout();		
+
+		m_frame.setTitle("SVM Training Data Generation Tool");
+		m_frame.setVisible(true);
+		m_frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		m_frame.setResizable(false);
+		m_frame.pack();
+	}
+	
+	public void setBackgroundImageAndRedraw(final BufferedImage img)
+	{
+		m_gridPanel.setBackgroundImageAndRedraw(img);
+		m_frame.pack();
+	}
+	
+	public void setGridDimensions(final int horizontalCells, final int verticalCells)
+	{
+		m_gridPanel.setGridDimensions(horizontalCells, verticalCells);
+	}
+	
+	public void setRegionOfInterest(final Pair<Integer, Integer> topLeft,
+                                    final Pair<Integer, Integer> bottomRight)
+	{
+		m_gridPanel.setRegionOfInterest(topLeft, bottomRight);
+	}
+	
+	public void setGridColor(final Color gridColor)
+	{
+		m_gridPanel.setGridColor(gridColor);
+	}
+	
+	public void setSelectedCellColor(final Color selectedCellColor)
+	{
+		m_gridPanel.setSelectedCellColor(selectedCellColor);
+	}
+	
+	public InteractiveGridPanel getGridPanel()
+	{
+		return m_gridPanel;
+	}
+
+	public void showEoFDialog() {
+		JOptionPane.showMessageDialog(m_frame,
+								     "End of video file has been reached!",
+								     "Finished!",
+								     JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	public void showWriteErrorDialog()
+	{
+		JOptionPane.showMessageDialog(m_frame,
+									  "Could not write files to disk!",
+									  "I/O Error",
+									  JOptionPane.ERROR_MESSAGE);
+		System.exit(0);
+	}
+
+	public void showInvalidOutputPathErrorDialog()
+	{
+		JOptionPane.showMessageDialog(m_frame,
+									  "Supplied output path(s) either invalid or nonexistent",
+									  "Invalid Output Path!",
+									  JOptionPane.ERROR_MESSAGE);
+		System.exit(0);
+	}
+	
+	private void configureLayout()
+	{
+		Dimension preferredSize = new Dimension(150, 30);
+		
+		JMenuBar menuBar = createMenuBar();
+		JButton generateButton = createGenerateButton(preferredSize);
+		JButton nextButton = createNextButton(preferredSize);
+		
+		m_frame.setLayout(new BorderLayout());
+		m_frame.add(menuBar, BorderLayout.NORTH);		
+		m_frame.add(m_gridPanel);
+		
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new FlowLayout());
+		buttonPanel.add(generateButton);
+		buttonPanel.add(nextButton);
+		m_frame.add(buttonPanel, BorderLayout.SOUTH);
+	}
+	
+	private JMenuBar createMenuBar()
+	{
+		JMenuBar menuBar = new JMenuBar();
+		
+		JMenu fileMenu = new JMenu("File");
+		JMenuItem openItem = createFileMenuItem();
+				
+		fileMenu.add(openItem);
+	
+		JMenu settingsMenu = new JMenu("Settings");		
+		JMenu gridSubMenu = createGridDimensionSubMenu();		
+		JMenuItem gridColorItem = createGridColorMenuItem();		
+		JMenuItem gridHighlightColorItem = createHighlightColorMenuItem();		
+		JMenuItem frameSkipItem = createFrameSkipMenuItem();
+		
+	    settingsMenu.add(gridSubMenu);
+	    settingsMenu.add(gridColorItem);
+	    settingsMenu.add(gridHighlightColorItem);
+	    settingsMenu.add(frameSkipItem);	    
+		
+		menuBar.add(fileMenu);
+		menuBar.add(settingsMenu);
+		
+		return menuBar;
+	}
+	
+	private JButton createGenerateButton(final Dimension preferredSize)
+	{
+		JButton generateButton = new JButton();
+		generateButton.setPreferredSize(preferredSize);
+		generateButton.setText("Generate Images");
+		
+		generateButton.addActionListener(new ActionListener()
+		                               	     {
+			                           	         public void actionPerformed(ActionEvent e)
+			                           	         {
+			                           	             if(!m_parentApplication.writeImagesToDisk(m_gridPanel.getTopLeftRoI(),
+			                           	            		                                   m_gridPanel.getBottomRightRoI()))
+			                           	             {
+			                           	                 showWriteErrorDialog();
+			                           	             }
+			                           	             else
+			                           	             {
+				                           	             m_parentApplication.displayNextFrame();                          	        	   
+			                           	             }
+			                           	         }
+		                               	     }
+		                                 );		
+		return generateButton;
+	}
+	
+	private JButton createNextButton(final Dimension preferredSize)
+	{
+		JButton nextButton = new JButton();
+		nextButton.setPreferredSize(preferredSize);
+		nextButton.setText("Next Frame");
+		
+		nextButton.addActionListener(new ActionListener()
+								  	     {
+								  	         public void actionPerformed(ActionEvent e)
+								  	         {
+								  	        	 boolean displayNext = true;
+								  	        	 
+								  	        	 if(m_gridPanel.getSelectedCells().size() > 0)
+								  	        	 {
+								  	        		 String message = "There are selected grid cells. If you continue, no training files will be written!";
+								  	        		 int decision = JOptionPane.showConfirmDialog(m_frame, 
+								  	        				                                      message,
+								  	        				                                      "Warning",
+								  	        				                                      JOptionPane.YES_NO_OPTION);
+								  	        		 if(decision == JOptionPane.NO_OPTION)
+								  	        		 {
+								  	        			 displayNext = false;
+								  	        		 }
+								  	        	 }
+								  	        	 
+								  	        	 if(displayNext)
+								  	        	 {
+									  	        	 m_parentApplication.displayNextFrame();
+								  	        	 }		                           	           
+								  	         } 
+								  	     } 
+								     );		
+		return nextButton;
+	}
+	
+	private JMenuItem createFileMenuItem()
+	{
+		JMenuItem openItem = new JMenuItem("Open");
+		openItem.addActionListener(new ActionListener()
+				                       {
+										   @Override
+										   public void actionPerformed(ActionEvent arg0)
+										   {
+											   JFileChooser fc = new JFileChooser("Select Input File");
+											   fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+											   FileNameExtensionFilter videoFormats = new FileNameExtensionFilter("Video Formats",
+																                                                  "avi",
+																                                                  "mov",
+																                                                  "mp4",
+																                                                  "mpeg",
+																                                                  "mpg",
+																                                                  "wmv");
+											   fc.addChoosableFileFilter(videoFormats);
+											   fc.setFileFilter(videoFormats);
+											   
+											   if(fc.showOpenDialog(m_frame) == JFileChooser.APPROVE_OPTION)
+											   {
+												   m_parentApplication.setSourceFile(fc.getSelectedFile().getPath());
+											   }
+									       }				                       
+				                       }
+				                  );
+		return openItem;
+	}
+	
+	private JMenuItem createGridColorMenuItem()
+	{
+		JMenuItem gridColorItem = new JMenuItem("Grid Color");
+		gridColorItem.addActionListener(new ActionListener()
+				                            {
+												@Override
+												public void actionPerformed(ActionEvent arg0)
+												{
+													Color color = JColorChooser.showDialog(m_frame,
+															                               "Grid Color",
+															                               InteractiveGridPanel.DEFAULT_GRID_COLOR);
+													m_gridPanel.setGridColor(color);
+												}
+				                            }
+				                       );
+		return gridColorItem;
+	}
+	
+	private JMenuItem createHighlightColorMenuItem()
+	{
+		JMenuItem gridHighlightColorItem = new JMenuItem("Grid Highlight Color");
+		gridHighlightColorItem.addActionListener(new ActionListener()
+											         {
+													  	 @Override
+														 public void actionPerformed(ActionEvent arg0)
+														 {
+															 Color color = JColorChooser.showDialog(m_frame,
+																	                                "Grid Highlight Color",
+																	                                InteractiveGridPanel.DEFAULT_SELECTED_CELL_COLOR);
+															 m_gridPanel.setSelectedCellColor(color);
+														 }
+											         }
+                                                );
+		return gridHighlightColorItem;
+	}
+	
+	private JMenuItem createFrameSkipMenuItem()
+	{
+		JMenuItem frameSkipItem = new JMenuItem("Frame Skip");
+		frameSkipItem.addActionListener(new ActionListener()
+									        {
+											  	@Override
+												public void actionPerformed(ActionEvent arg0)
+												{
+											  		String stringInput = null;
+											  		int framesToSkip = Integer.MIN_VALUE;
+											  		
+											  		while(framesToSkip < 0)
+											  		{
+											  			stringInput = JOptionPane.showInputDialog("Enter number of frames to skip.");
+											  			try{
+											  				framesToSkip = Integer.parseInt(stringInput);
+											  				
+											  				if(framesToSkip < 0)
+											  				{
+											  					JOptionPane.showMessageDialog(m_frame, "Value entered must be positive.");
+											  				}
+											  			}
+											  			catch(NumberFormatException e)
+											  			{
+											  				JOptionPane.showMessageDialog(m_frame, "Value entered is not a number.");
+											  			}
+											  		}
+											  		
+											  		m_parentApplication.setFrameSkip(framesToSkip);
+												}
+									        }
+                                       );
+		return frameSkipItem;
+	}
+	
+	private JMenu createGridDimensionSubMenu()
+	{
+		JMenu gridDimensionMenu = new JMenu("Grid Dimensions");
+		ButtonGroup dimensionButtons = new ButtonGroup();
+		
+		JRadioButtonMenuItem twentyByTwenty = new JRadioButtonMenuItem("96 x 54");
+		JRadioButtonMenuItem thirtyByThirty = new JRadioButtonMenuItem("64 x 36", true);
+		JRadioButtonMenuItem fortyByForty = new JRadioButtonMenuItem("48 x 27");
+		JRadioButtonMenuItem sixtyBySixty = new JRadioButtonMenuItem("32 x 18");
+		JRadioButtonMenuItem oneTwentyByOneTwenty = new JRadioButtonMenuItem("16 x 9");
+		
+		ActionListener radioListener = new ActionListener()
+										   {
+										       @Override
+											   public void actionPerformed(ActionEvent arg0)
+											   {
+											       AbstractButton radioOption = (AbstractButton) arg0.getSource();
+												   String label = radioOption.getText();
+												   String[] dimensions = label.split(" x ");
+												   m_gridPanel.setGridDimensions(Integer.parseInt(dimensions[0]),
+														                         Integer.parseInt(dimensions[1]));												   
+											   }													
+										   };
+										   
+	    twentyByTwenty.addActionListener(radioListener);
+	    thirtyByThirty.addActionListener(radioListener);
+	    fortyByForty.addActionListener(radioListener);
+	    sixtyBySixty.addActionListener(radioListener);
+	    oneTwentyByOneTwenty.addActionListener(radioListener);
+	    
+	    dimensionButtons.add(twentyByTwenty);
+	    dimensionButtons.add(thirtyByThirty);
+	    dimensionButtons.add(fortyByForty);
+	    dimensionButtons.add(sixtyBySixty);
+	    dimensionButtons.add(oneTwentyByOneTwenty);
+	    
+	    gridDimensionMenu.add(twentyByTwenty);
+	    gridDimensionMenu.add(thirtyByThirty);
+	    gridDimensionMenu.add(fortyByForty);
+	    gridDimensionMenu.add(sixtyBySixty);
+	    gridDimensionMenu.add(oneTwentyByOneTwenty);
+	    
+	    return gridDimensionMenu;
+	}
+}
