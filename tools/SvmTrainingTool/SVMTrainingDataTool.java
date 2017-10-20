884a79a30b1411711cd542d74d85ce6be64f5dd5
Derek Kumagai <Derek.kumagai@elektrobit.com>
2017-10-20 14:57:55 -0700

    replaced tabs with spaces because, FUCKING ECLIPSE SHIT!

--- tools/SvmTrainingTool/SVMTrainingDataTool.java
+++ tools/SvmTrainingTool/SVMTrainingDataTool.java
@@ -4,16 +4,16 @@ import core.SVMTrainingDataToolApplication;
 
 public class SVMTrainingDataTool {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		System.loadLibrary( Core.NATIVE_LIBRARY_NAME);
-		
-		SVMTrainingDataToolApplication app = new SVMTrainingDataToolApplication();
-		app.setSourceFile("C:/Users/kumader/Downloads/simpsons_movie_1080p_hddvd_trailer/2.mp4");
-		app.setPositiveOutputPath("E:/SVMTraining/positive");
-		app.setNegativeOutputPath("E:/SVMTraining/negative");		
-		//app.setSourceFile(args[0]);
-		//app.setPositiveOutputPath(args[1]);
-		//app.setNegativeOutputPath(args[2]);
-	}
+        System.loadLibrary( Core.NATIVE_LIBRARY_NAME);
+        
+        SVMTrainingDataToolApplication app = new SVMTrainingDataToolApplication();
+        app.setSourceFile("C:/Users/kumader/Downloads/simpsons_movie_1080p_hddvd_trailer/2.mp4");
+        app.setPositiveOutputPath("E:/SVMTraining/positive");
+        app.setNegativeOutputPath("E:/SVMTraining/negative");        
+        //app.setSourceFile(args[0]);
+        //app.setPositiveOutputPath(args[1]);
+        //app.setNegativeOutputPath(args[2]);
+    }
 }
